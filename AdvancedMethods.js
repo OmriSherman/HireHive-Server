e9c620e682f5479e9188deef1e45088800de366d
Omri Sherman <omrishermansakana@gmail.com>
2023-07-13 00:54:31 +0300

    added nodemon and finished login function.

--- AdvancedMethods.js
+++ AdvancedMethods.js
@@ -50,6 +50,58 @@ appRouter.get("/insertNewUser", (req, resp) => {
 });
 
 
+appRouter.post('/loginUser', jsonParser, (req, resp) => {
+  const { email, password } = req.body;
+
+  let candidatesQuery = `SELECT * FROM candidates WHERE email='${email}' AND password='${password}'`;
+  db.query(candidatesQuery, (err1, result1) => {
+    if (err1) {
+      console.error(err1);
+      resp.status(500).send('An error occurred while logging in.');
+    } else if (result1.length > 0) {
+      resp.json(result1[0]);
+    } else {
+      let employersQuery = `SELECT * FROM employers WHERE email='${email}' AND password='${password}'`;
+      db.query(employersQuery, (err2, result2) => {
+        if (err2) {
+          console.error(err2);
+          resp.status(500).send('An error occurred while logging in.');
+        } else if (result2.length > 0) {
+          resp.json(result2[0]);
+        } else {
+          resp.status(400).send('Invalid email or password.');
+        }
+      });
+    }
+  });
+});
+
+// appRouter.post("/loginUser", jsonParser, async (req, resp) => {
+//   try {
+//     const { email, password } = req.body;
+//     const candidateQuery = `SELECT * FROM candidates WHERE email=? AND password=?`;
+//     const employerQuery = `SELECT * FROM employers WHERE email=? AND password=?`;
+
+//     // Check in candidates table
+//     const candidateResult = await db.query(candidateQuery, [email, password]);
+//     if (candidateResult.length > 0) {
+//       return resp.json(candidateResult);
+//     }
+
+//     // Check in employers table
+//     const employerResult = await db.query(employerQuery, [email, password]);
+//     if (employerResult.length > 0) {
+//       return resp.json(employerResult);
+//     }
+
+//     // Neither candidate nor employer found
+//     return resp.status(404).json({ error: 'Invalid email or password' });
+//   } catch (err) {
+//     console.error(err);
+//     return resp.status(500).send('An error occurred while logging in');
+//   }
+// });
+
 
 
 module.exports = appRouter;
